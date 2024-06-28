c12f3ecde0519e64a4c8770dad946abc08af4890
yaejin12 <pos03167@naver.com>
2024-06-28 12:16:37 +0900

    jpa study

--- src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
@@ -21,6 +21,8 @@ public class Idol {
 
     private int age;
 
+    private String gender;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
@@ -34,8 +36,19 @@ public class Idol {
         }
     }
 
+    public Idol(String idolName, int age,String gender , Group group) {
+        this.idolName = idolName;
+        this.age = age;
+        this.gender = gender;
+        if (group != null) {
+            changeGroup(group);
+        }
+    }
+
     public void changeGroup(Group group) {
         this.group = group;
         group.getIdols().add(this);
     }
+
+
 }
